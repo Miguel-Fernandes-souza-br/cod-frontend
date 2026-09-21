5033da9d41026699eba40abd74efc280c668e7f7
MIGUEL FERNANDES DE SOUZA <miguel_f_de-souza@estudante.sesisenai.org.br>
2026-09-21 15:57:25 -0300

    feito

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -3,8 +3,145 @@ import './App.css'
 
 function App() {
 
- 
+function relatorio() {
+    let pf = 40, pj = 33;
+    let tempoPF = 12, tempoPJ = 42;
+    let valorPF = 2350, valorPJ = 8900;
 
+    alert(
+        "Total relatórios: " + (pf + pj) +
+        "\nTempo total: " + (tempoPF + tempoPJ) + "h" +
+        "\nValor total: R$ " + (valorPF + valorPJ) +
+        "\nMédia PF: R$ " + (valorPF / pf).toFixed(2) +
+        "\nMédia PJ: R$ " + (valorPJ / pj).toFixed(2) +
+        "\nTempo médio PF: " + (tempoPF / pf).toFixed(2) + "h" +
+        "\nTempo médio PJ: " + (tempoPJ / pj).toFixed(2) + "h"
+    );
+}
+
+
+function calcularFreela() {
+    let horas = Number(prompt("Horas estimadas:"));
+
+    let preco = 500 + (horas * 350);
+    let lucro = preco - 500;
+
+    alert("Preço: R$ " + preco.toFixed(2) +
+          "\nLucro: R$ " + lucro.toFixed(2));
+}
+
+
+function calcularPrompt() {
+    let caracteres = Number(prompt("Número de caracteres:"));
+    let custoToken = Number(prompt("Custo de cada token:"));
+
+    let tokens = 5 + caracteres;
+    let custo = tokens * custoToken;
+
+    alert("Tokens gastos: " + tokens +
+          "\nCusto: R$ " + custo.toFixed(2));
+}
+
+
+function calcularLucro() {
+    let caminhoes = Number(prompt("Número de caminhões:"));
+
+    let jarés = caminhoes * 50;
+    let vendas = jarés * 90;
+    let frete = caminhoes * 450;
+    let lucro = vendas - frete;
+
+    alert("O lucro foi: R$ " + lucro.toFixed(2));
+}
+
+
+function calcularChurrasco() {
+    let pessoas = Number(prompt("Digite o número de pessoas:"));
+
+    let carne = pessoas * 0.5;
+    let cerveja = pessoas * 1;
+    let agua = pessoas * 0.5;
+    let refri = pessoas * 0.2;
+
+    alert("Carne: " + carne.toFixed(1) + " kg" +
+          "\nCerveja: " + cerveja.toFixed(1) + " L" +
+          "\nÁgua: " + agua.toFixed(1) + " L" +
+          "\nRefri: " + refri.toFixed(1) + " L");
+}
+
+
+function calcularRacao() {
+    let gramas = Number(prompt("Digite o peso da ração em gramas:"));
+
+    let preco = (gramas / 1000) * 10;
+
+    alert("O preço da ração é: R$ " + preco.toFixed(2));
+}
+
+
+function calcularPrecoObra() {
+    let precoCompra = Number(prompt("Digite quanto Romero pagou pela obra:"));
+
+    let precoVenda = precoCompra + (precoCompra * 2);
+
+    alert("O preço de venda da obra é: R$ " + precoVenda.toFixed(2));
+}
+
+
+function calcularPoupancaManoJuca() {
+    let salario = Number(prompt("Digite o valor do salário:"));
+    let moradia = Number(prompt("Digite o valor da moradia:"));
+    let agua = Number(prompt("Digite o valor da água:"));
+    let luz = Number(prompt("Digite o valor da luz:"));
+    let internet = Number(prompt("Digite o valor da internet:"));
+    let gasolina = Number(prompt("Digite o valor da gasolina:"));
+    let streamings = Number(prompt("Digite o valor dos streamings:"));
+    let telefone = Number(prompt("Digite o valor do telefone:"));
+    let outros = Number(prompt("Digite o valor de outros gastos:"));
+
+    let contas = moradia + agua + luz + internet + gasolina + streamings + telefone + outros;
+    let sobra = salario - contas;
+
+    alert("Com muito carinho, Mano Juca, você conseguiu guardar: R$ " + sobra.toFixed(2));
+}
+
+
+  function calcularBombasSarumano() {
+    let shows = Number(prompt("Digite a quantidade de shows marcados:"));
+    let precoBomba = Number(prompt("Digite o preço unitário da bomba de fumaça:"));
+
+    let bombas = shows * 7;
+    let dinheiro = bombas * precoBomba;
+
+    alert("Sarumano precisa comprar " + bombas + " bombas de fumaça." +
+          "\nEle precisa mobilizar: R$ " + dinheiro.toFixed(2));
+}
+
+
+function calcularLucroCapitaoGanso() {
+    let suprimentos = Number(prompt("Digite o valor gasto com suprimentos e mercadorias:"));
+    let ingressos = Number(prompt("Digite o faturamento com venda de ingressos:"));
+    let itens = Number(prompt("Digite o faturamento com venda de itens:"));
+
+    let faturamento = ingressos + itens;
+    let lucro = faturamento - suprimentos;
+    let lucroPercentual = (lucro / faturamento) * 100;
+
+    alert("O lucro obtido foi: R$ " + lucro.toFixed(2) +
+          "\nO lucro percentual foi: " + lucroPercentual.toFixed(2) + "%");
+}
+
+ 
+function calcularLucroDonaBete() {
+    let bruto = Number(prompt("Digite o valor bruto das apostas:"));
+    let premiacoes = Number(prompt("Digite o valor gasto com premiações:"));
+    let presentes = Number(prompt("Digite o valor gasto com presentes e agrados:"));
+    let comissoes = Number(prompt("Digite o valor gasto com comissões:"));
+
+    let lucro = bruto - premiacoes - presentes - comissoes;
+
+    alert("O lucro da Dona Bete foi: R$ " + lucro.toFixed(2));
+}
 
 
   function calcularFreteTelles() {
@@ -113,8 +250,20 @@ alert("A força de trabalho da Macrohard conta com um total de " + totalDevs + "
       <button onClick={SalarioJunin}>Salario junin</button>
       <button onClick={Calcularcarga}>Carga</button>
       <button onClick={calcularChanceDev}>monika</button>
+     <hr />
       <button onClick={calcularFreteTelles}>Talles2</button>
-
+      <button onClick={calcularLucroDonaBete}>Donabete</button>
+      <button onClick={calcularLucroCapitaoGanso}>Ganso</button>
+      <button onClick={calcularBombasSarumano}>Sarumano</button>
+      <button onClick={calcularPoupancaManoJuca}>mano juca</button>
+      <button onClick={ calcularPrecoObra}>Obra</button>
+      <button onClick={calcularRacao}>raçao</button>
+      <button onClick={calcularChurrasco}>Churrasco</button>
+      <hr />
+      <button onClick={calcularLucro}>Lucro jacare</button>
+      <button onClick={calcularPrompt}>Prompt</button>
+      <button onClick={calcularFreela}>Freela</button>
+      <button onClick={relatorio}>Relatorio</button>
       <hr />
 
 
